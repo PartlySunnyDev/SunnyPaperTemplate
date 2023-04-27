4bf3e5083f2a92d6037a27cb610d165e0f4e166a
Steven Zhu <65844236+steveyxz@users.noreply.github.com>
2023-04-27 10:29:38 +1000

    Initial commit

---
+++ src/main/java/me/partlysunny/sunnypapertemplate/MyPlugin.java
@@ -0,0 +1,32 @@
+package me.partlysunny.sunnypapertemplate;
+
+import me.partlysunny.commandeer.CommandManager;
+import me.partlysunny.commons.paper.SunnyPlugin;
+import org.jetbrains.annotations.NotNull;
+
+public final class MyPlugin extends SunnyPlugin {
+
+    public MyPlugin() {
+        super("MyPlugin");
+    }
+
+    @Override
+    public void registerCommands(@NotNull CommandManager commandManager) {
+
+    }
+
+    @Override
+    public void registerConfigurations() {
+
+    }
+
+    @Override
+    public void registerGuis() {
+
+    }
+
+    @Override
+    public void registerListeners() {
+
+    }
+}
